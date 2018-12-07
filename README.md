90cb89146a5abcdb403c0c4423ddd15c590b6dda
Mihael Haluga <mihael.haluga@thespian.hr>
2018-12-07 15:18:02 +0100

    Added firebase deploy, readme update, small fixes

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # firestore-test
 
-## Simple application for sending events (messages) to firestore and listenitng to changes
+## Simple application for sending events (messages) to firestore and listening to changes
 
-To test application as event `Emitter` visit in one browser window:
+To test application as event `Emitter` visit:
+https://notifying-test.firebaseapp.com/#/emitter
 
-And as `Listener` visit in second browser window (or different PC!): 
+And as `Listener` visit in second browser window (or different PC!): https://notifying-test.firebaseapp.com/#/listener
 
 ### Project setup
 ```
--- src/Listener.vue
+++ src/Listener.vue
@@ -21,33 +21,27 @@ export default {
     unsubscribe = firestore.collection('events').where('added', '>', this.initDate)
       .onSnapshot(snapshot => {
         snapshot.docChanges().forEach(item => {
-          console.log(item)
           const data = item.doc.data()
           if (item.type === 'added') {
             this.$Notice.success({
               title: data.type,
               desc: 'Added event!'
             })
-          } else if (item.type === 'removed') {
+          }
+        })
+      })
+    unsubscribeDelete = firestore.collection('events')
+      .onSnapshot(snapshot => {
+        snapshot.docChanges().forEach(item => {
+          if (item.type === 'removed') {
+            const data = item.doc.data()
             this.$Notice.warning({
               title: data.type,
-              desc: 'Added removed!'
+              desc: 'Removed event!'
             })
           }
         })
       })
-      unsubscribeDelete = firestore.collection('events')
-        .onSnapshot(snapshot => {
-          snapshot.docChanges().forEach(item => {
-            if (item.type === 'removed') {
-              const data = item.doc.data()
-              this.$Notice.warning({
-                title: data.type,
-                desc: 'Removed event!'
-              })
-            }
-          })
-        })
   },
   beforeDestroy() {
     unsubscribe && unsubscribe()
